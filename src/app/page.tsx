b61b5c02e39e53155ca60675b85d762fe2e43221
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-01-03 23:35:41 +0100

    [UPDATE] added new component to HomePage: Presentation.tsx, Presentation1.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import ProgressBar from "./componets/layout/ProgressBar";
+// import ProgressBar from "./componets/layout/ProgressBar";
 import NavBar from "./componets/layout/NavBar";
+import Presentation1 from "./componets/layout/Presentation1";
+import Presentation from "./componets/layout/Presentation";
 
 export default function Home() {
   return (
-      <header>  
+    <div>
         <NavBar />
-        <ProgressBar />
-      </header>
+        {/* <ProgressBar /> */}
+        <div className="grid grid-cols-2"> 
+          <div><Presentation /></div>       
+          <div><Presentation1 /></div>
+        </div>
+
+    </div>    
   );
 }
