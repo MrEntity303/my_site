90c8b1b885e5e9207550cefde2d041d1491f081d
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-01-03 19:57:52 +0100

    [CREATE] create new component Presentation.txs, Presentation1.txs

---
+++ src/app/componets/layout/Presentation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Presentation = () => {
+    return (
+        <div>
+            <h1>Welcome to My Site</h1>
+            <p>This is the presentation component.</p>
+        </div>
+    );
+};
+
+export default Presentation;
---
+++ src/app/componets/layout/Presentation1.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Presentation1 = () => {
+    return (
+        <div>
+            <h1>Welcome to Presentation1</h1>
+            <p>This is a sample presentation component.</p>
+        </div>
+    );
+};
+
+export default Presentation1;
