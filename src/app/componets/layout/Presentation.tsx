6c60731c69a4a2996f1b677b5911586f307db7bf
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-01-06 22:56:03 +0100

    [FIX & ADD] fixed responsive feacture and add new gray color cards

--- src/app/componets/layout/Presentation.tsx
+++ src/app/componets/layout/Presentation.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-
+'use client';
+import React, { useState, useEffect} from 'react';
+ 
 const Presentation = () => {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setIsScrolling(true);
+      } else {
+        setIsScrolling(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event when the component is unmounted
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, );
+
     return (
-<main id="home" className=" flex flex-col-reverse lg:flex-row justify-center items-center">
+<main id="home" className=" flex flex-col-reverse pt-16 lg:flex-row justify-center items-center lg:h-screen">
   <div className="flex w-full lg:w-1/2 justify-center items-center">
     <div className="text-center p-3">
       <div className="flex justify-center items-center space-x-2">
@@ -22,23 +42,15 @@ const Presentation = () => {
   <div className="flex w-full lg:w-1/2 justify-center items-center p-3">
     <img src="..\img\profile1.jpg" alt="profile" className='rounded-full w-1/2 h-1/2 border-4 border-sky-400'/>
   </div>
+  <a href="#about" className={`absolute bottom-4 flex flex-col justify-center items-center transition-opacity duration-300 ${
+          isScrolling ? 'hidden' : 'block'
+        }`}>
+    <span className="text-2xl animate-pulseGrow">👇</span>
+    <span>Scroll Down</span>
+  </a>
 </main>
 
     );
 };
 
 export default Presentation;
-
-        // <div className='mt-40'>
-        // <div className="flex justify-center items-center space-x-2">	
-        //     <span className='text-2xl'>Ciao, benvenuto (</span>
-        //     <img src="./img/wave-hello.gif" alt="hi" className='w-10 ml-2'/>
-        //     <span className="text-2xl">) , Io sono</span>
-        // </div>
-        // <div className="flex justify-center items-center">
-        //     <h1 className="font-bold text-6xl">Patryk Bialowas</h1>
-        // </div>
-        // <div className="flex justify-center items-center">
-        //     <span className='text-2xl'>Enthusiast Developer</span>
-        // </div>
-        // </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,21 @@ export default {
   ],
   theme: {
     extend: {
+      keyframes: {
+        pulseGrow: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        customGray: '#16161A'
+        customGray: '#16161A',
+        grayCard: '#242629'
       },
+    },
+    animation: {
+      pulseGrow: "pulseGrow 1s infinite",
     },
   },
   plugins: [],
