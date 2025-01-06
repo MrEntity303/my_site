26975592f9df039b513ed675ed828b16f460c2ce
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-01-06 17:31:23 +0100

    [FIX] presentation component responsivity

--- src/app/componets/layout/Presentation.tsx
+++ src/app/componets/layout/Presentation.tsx
@@ -2,20 +2,43 @@ import React from 'react';
 
 const Presentation = () => {
     return (
-        <div className='mt-40'>
-        <div className="flex justify-center items-center space-x-2">	
-            <span className='text-2xl'>Ciao, benvenuto (</span>
-            <img src="./img/wave-hello.gif" alt="hi" className='w-10 ml-2'/>
-            <span className="text-2xl">) , Io sono</span>
-        </div>
-        <div className="flex justify-center items-center">
-            <h1 className="font-bold text-6xl">Patryk Bialowas</h1>
-        </div>
-        <div className="flex justify-center items-center">
-            <span className='text-2xl'>Enthusiast Developer</span>
-        </div>
-        </div>
+<main className=" flex flex-col-reverse lg:flex-row justify-center items-center h-screen">
+  <div className="flex w-full lg:w-1/2 justify-center items-center">
+    <div className="text-center p-3">
+      <div className="flex justify-center items-center space-x-2">
+        <span className='text-2xl'>Ciao, benvenuto (</span>
+        <img src="./img/wave-hello.gif" alt="hi" className='w-10 ml-2'/>
+        <span className="text-2xl">) , Io sono</span>
+      </div>
+      <div className="flex justify-center items-center">
+        <h1 className="font-bold text-6xl">Patryk Bialowas</h1>
+      </div>
+      <div className="flex justify-center items-center">
+        <span className='text-2xl mt-2'>Enthusiast Developer</span>
+      </div>
+    </div>
+  </div>
+  
+  <div className="flex w-full lg:w-1/2 justify-center items-center p-3">
+    <img src="..\img\profile1.jpg" alt="profile" className='rounded-full w-1/2 h-1/2 border-4 border-sky-400'/>
+  </div>
+</main>
+
     );
 };
 
 export default Presentation;
+
+        // <div className='mt-40'>
+        // <div className="flex justify-center items-center space-x-2">	
+        //     <span className='text-2xl'>Ciao, benvenuto (</span>
+        //     <img src="./img/wave-hello.gif" alt="hi" className='w-10 ml-2'/>
+        //     <span className="text-2xl">) , Io sono</span>
+        // </div>
+        // <div className="flex justify-center items-center">
+        //     <h1 className="font-bold text-6xl">Patryk Bialowas</h1>
+        // </div>
+        // <div className="flex justify-center items-center">
+        //     <span className='text-2xl'>Enthusiast Developer</span>
+        // </div>
+        // </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
 // import ProgressBar from "./componets/layout/ProgressBar";
 import NavBar from "./componets/layout/NavBar";
-import Presentation1 from "./componets/layout/Presentation1";
 import Presentation from "./componets/layout/Presentation";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen scroll-m-2">
         <NavBar />
         {/* <ProgressBar /> */}
-        <div className="grid grid-cols-2"> 
-          <div><Presentation /></div>       
-          <div><Presentation1 /></div>
-        </div>
-
+        <div><Presentation /></div>
     </div>    
   );
 }
