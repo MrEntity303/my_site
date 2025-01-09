13534ff96d75821a5d219a043829855117e5cc73
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-01-09 17:23:53 +0100

    [CREATE] add skills section page

--- src/app/pages/skills/page.tsx
+++ src/app/pages/skills/page.tsx
@@ -1,39 +1,23 @@
+import skillsData from "@/ui/skills/skillsData";
+
 const Skills = () => {
-    return (
-        <section id="tech-skills" className="flex flex-col justify-start min-h-screen">
-            <h1 className="text-4xl flex flex-col justify-center items-center">Skills</h1>
-            <div className="grid grid-flow-col-dense gap-4 pt-5">
-
-                <div className="flex flex-col justify-center items-center w-24 h-24 bg-grayCard p-4 rounded-2xl">
-                    <div className="flex justify-center items-center">
-                        <img src="../card_svg/icons8-python.svg" alt="python" />
-                    </div>
-                    <span>Linguaggio</span>
-                </div>
-
-                <div className="flex flex-col justify-center items-center w-24 h-24 bg-grayCard p-4 rounded-2xl">
-                    <div className="flex justify-center items-center">
-                        <img src="../card_svg/icons8-python.svg" alt="python" />
-                    </div>
-                    <span>Linguaggio</span>
-                </div>
-
-                <div className="flex flex-col justify-center items-center w-24 h-24 bg-grayCard p-4 rounded-2xl">
-                    <div className="flex justify-center items-center">
-                        <img src="../card_svg/icons8-python.svg" alt="python" />
-                    </div>
-                    <span>Linguaggio</span>
-                </div>
-
-                <div className="flex flex-col justify-center items-center w-24 h-24 bg-grayCard p-4 rounded-2xl">
-                    <div className="flex justify-center items-center">
-                        <img src="../card_svg/icons8-python.svg" alt="python" />
-                    </div>
-                    <span>Linguaggio</span>
-                </div>
-
+  return (
+    <section id="tech-skills" className="min-h-screen flex justify-center items-center flex-col">
+      <h1 className="text-3xl font-bold text-center p-8">Skills</h1>
+      <div className="flex flex-wrap justify-center gap-4 xl:px-64">
+        {skillsData.map((skill, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center bg-grayCard p-4 rounded-lg w-24 h-24 group hover:bg-white hover:scale-105 transition-transform duration-300"
+          >
+            <div className="flex justify-center items-center w-10 h-10 group-hover:scale-125 transition-transform duration-300">
+              <img src={skill.imgSrc} alt={skill.name} />
             </div>
-        </section>
-    );
-  };
-  export default Skills;
+            <span className="mt-2 text-sm font-medium group-hover:text-black">{skill.name}</span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+export default Skills;
---
+++ src/app/ui/skills/skillsData.tsx
@@ -0,0 +1,119 @@
+// src/data/skillsData.js
+
+const skillsData = [
+    {
+      name: "Vue",
+      imgSrc: "https://img.icons8.com/?size=100&id=rY6agKizO9eb&format=png&color=000000",
+    },
+    {
+      name: "Python",
+      imgSrc: "https://img.icons8.com/?size=100&id=13441&format=png&color=000000",
+    },
+    {
+      name: "Java",
+      imgSrc: "https://img.icons8.com/?size=100&id=13679&format=png&color=000000",
+    },
+    {
+      name: "Node.js",
+      imgSrc: "https://img.icons8.com/?size=100&id=54087&format=png&color=000000",
+    },
+    {
+      name: "JavaScript",
+      imgSrc: "https://img.icons8.com/?size=100&id=108784&format=png&color=000000",
+    },
+    {
+      name: "React",
+      imgSrc: "https://img.icons8.com/?size=100&id=123603&format=png&color=000000",
+    },
+    {
+      name: "Tailwind",
+      imgSrc: "https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000",
+    },
+    {
+      name: "HTML5",
+      imgSrc: "https://img.icons8.com/?size=100&id=20909&format=png&color=000000",
+    },
+    {
+      name: "CSS3",
+      imgSrc: "https://img.icons8.com/?size=100&id=21278&format=png&color=000000",
+    },
+    {
+      name: "Express",
+      imgSrc: "https://img.icons8.com/?size=100&id=9Gfx4Dfxl0JK&format=png&color=000000",
+    },
+    {
+      name: "Bootstrap",
+      imgSrc: "https://img.icons8.com/?size=100&id=PndQWK6M1Hjo&format=png&color=000000",
+    },
+    {
+      name: "TypeScript",
+      imgSrc: "https://img.icons8.com/?size=100&id=wpZmKzk11AzJ&format=png&color=000000",
+    },
+    {
+      name: "SpringBoot",
+      imgSrc: "https://img.icons8.com/?size=100&id=90519&format=png&color=000000",
+    },
+    {
+      name: "MongoDB",
+      imgSrc: "https://img.icons8.com/?size=100&id=bosfpvRzNOG8&format=png&color=000000",
+    },
+    {
+      name: "MySQL",
+      imgSrc: "https://img.icons8.com/?size=100&id=UFXRpPFebwa2&format=png&color=000000",
+    },
+    {
+      name: "GitHub",
+      imgSrc: "https://img.icons8.com/?size=100&id=LoL4bFzqmAa0&format=png&color=000000",
+    },
+    {
+      name: "Git",
+      imgSrc: "https://img.icons8.com/?size=100&id=20906&format=png&color=000000",
+    },
+    {
+      name: "Docker",
+      imgSrc: "https://img.icons8.com/?size=100&id=22813&format=png&color=000000",
+    },
+    {
+      name: "Pandas",
+      imgSrc: "https://img.icons8.com/?size=100&id=xSkewUSqtErH&format=png&color=000000",
+    },
+    {
+      name: "C++",
+      imgSrc: "https://img.icons8.com/?size=100&id=40669&format=png&color=000000",
+    },
+    {
+      name: "C",
+      imgSrc: "https://img.icons8.com/?size=100&id=40670&format=png&color=000000",
+    },
+    {
+      name: "Intellij",
+      imgSrc: "https://img.icons8.com/?size=100&id=61466&format=png&color=000000",
+    },
+    {
+      name: "JetBrains",
+      imgSrc: "https://img.icons8.com/?size=100&id=pj15SuHu3Vlt&format=png&color=000000",
+    },
+    {
+      name: "PostGress",
+      imgSrc: "https://img.icons8.com/?size=100&id=38561&format=png&color=000000",
+    },
+    {
+      name: "VsCode",
+      imgSrc: "https://img.icons8.com/?size=100&id=9OGIyU8hrxW5&format=png&color=000000",
+    },
+    {
+      name: "AutoCad",
+      imgSrc: "../card_svg/autocad.png",
+    },
+    {
+      name: "Inventor",
+      imgSrc: "../card_svg/inventor_icon.png",
+    },
+    {
+      name: "Next.js",
+      imgSrc: "https://img.icons8.com/?size=100&id=AU6Wc7r56Fxz&format=png&color=000000",
+    }
+  ];
+  
+  export default skillsData;
+  
