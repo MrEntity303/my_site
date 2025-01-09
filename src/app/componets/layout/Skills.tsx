8e2df77508f9aa643a5023058b9f0a2352887e39
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-01-09 09:27:10 +0100

    [CREATE] add Skills page

---
+++ src/app/componets/layout/Skills.tsx
@@ -0,0 +1,39 @@
+const Skills = () => {
+    return (
+        <section id="tech-skills" className="flex flex-col justify-between">
+            <h1 className="text-4xl flex flex-col justify-center items-center">Skills</h1>
+            <div className="grid grid-flow-col-dense gap-4 pt-5">
+
+                <div className="flex flex-col justify-center items-center w-24 h-24 bg-grayCard p-4 rounded-2xl">
+                    <div className="flex justify-center items-center">
+                        <img src="../card_svg/icons8-python.svg" alt="python" />
+                    </div>
+                    <span>Linguaggio</span>
+                </div>
+
+                <div className="flex flex-col justify-center items-center w-24 h-24 bg-grayCard p-4 rounded-2xl">
+                    <div className="flex justify-center items-center">
+                        <img src="../card_svg/icons8-python.svg" alt="python" />
+                    </div>
+                    <span>Linguaggio</span>
+                </div>
+
+                <div className="flex flex-col justify-center items-center w-24 h-24 bg-grayCard p-4 rounded-2xl">
+                    <div className="flex justify-center items-center">
+                        <img src="../card_svg/icons8-python.svg" alt="python" />
+                    </div>
+                    <span>Linguaggio</span>
+                </div>
+
+                <div className="flex flex-col justify-center items-center w-24 h-24 bg-grayCard p-4 rounded-2xl">
+                    <div className="flex justify-center items-center">
+                        <img src="../card_svg/icons8-python.svg" alt="python" />
+                    </div>
+                    <span>Linguaggio</span>
+                </div>
+
+            </div>
+        </section>
+    );
+  };
+  export default Skills;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-// import ProgressBar from "./componets/layout/ProgressBar";
 import NavBar from "./componets/layout/NavBar";
 import Presentation from "./componets/layout/Presentation";
 import About from "./componets/layout/About";
 import Experience from "./componets/layout/Experience";
-
 export default function Home() {
   return (
-    <div>
+    <div >
       <NavBar />
-      {/* <ProgressBar /> */}
       <Presentation />
       <About />
       <Experience />
