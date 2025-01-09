1376e3b39d2ce9bc22e5ca733fd73664d6145d3e
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-01-09 20:38:37 +0100

    [MINOR EDIT] index.valueof()

--- src/app/pages/skills/page.tsx
+++ src/app/pages/skills/page.tsx
@@ -7,7 +7,7 @@ const Skills = () => {
       <div className="flex flex-wrap justify-center gap-4 xl:px-64">
         {skillsData.map((skill, index) => (
           <div
-            key={index}
+            key={index.valueOf()}
             className="flex flex-col items-center bg-grayCard p-4 rounded-lg w-24 h-24 group hover:bg-white hover:scale-105 transition-transform duration-300"
           >
             <div className="flex justify-center items-center w-10 h-10 group-hover:scale-125 transition-transform duration-300">
