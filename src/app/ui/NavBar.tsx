9cf02657f7637ed1a7dc57120e8a10294b5a4d11
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-01-10 10:21:56 +0100

    [ADD] add CV

--- src/app/ui/NavBar.tsx
+++ src/app/ui/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     <nav className="bg-customGray fixed top-0 left-0 w-full flex justify-between items-center shadow-md">
       <div className="flex items-center">
         <img src="\img\logo_resized.png" alt="logo" className="w-16 h-16"/>
-        <button className="justify-end border border-white rounded-full font-bold hover:bg-white hover:text-blue-500 px-4 py-2">Download CV</button>
+        <a href="../dowload_resource/CV_Aggiornato_2025.pdf"><button className="justify-end border border-white rounded-full font-bold hover:bg-white hover:text-blue-500 px-4 py-2">Download CV</button></a>
       </div>
       <ul className="hidden lg:flex justify-end p-4 text-white text-xl space-x-14">
         <li><a href="#home" onClick={() => setActiveLink("#home")} className={`focus:text-sky-400 ${activeLink === "#home" ? "text-sky-400" : ""}`}>Home</a></li>
