90544268d2c12f527109e02e46f1e90039405768
MrEntity303 <patryk.bialowas@studenti.unicam.it>
2025-01-10 11:45:30 +0100

    [FIX] style button link

--- src/app/ui/link/LinkCard.tsx
+++ src/app/ui/link/LinkCard.tsx
@@ -7,9 +7,9 @@ interface LinkCardProps {
 }
 const LinkCard: React.FC<LinkCardProps> = ({ name, imgIcon, url }) => {
     return (
-        <a className="flex flex-col justify-center items-center bg-grayCard rounded-lg w-24 h-24 p-4" href={url}>
+        <a className="flex flex-col  items-center bg-grayCard p-4 rounded-lg w-24 h-24 group hover:bg-white hover:scale-105 transition-transform duration-300" href={url}>
             <img className="w-12 h-12 m-auto" src={imgIcon} alt="LinkedIn" />
-            <span className='font-extrabold text-gray-300'>{name}</span>
+            <span className='font-extrabold text-gray-300 group-hover:text-customGray'>{name}</span>
         </a>
     );
 };
